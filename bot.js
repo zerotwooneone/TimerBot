2dc6d382899c981190f2ce73d132f73ea8d80676
David Squire <squiredd@gmail.com>
2019-01-24 18:06:17 -0800

    tracking timers

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 // Configure logger settings
+var timerHash = {};
+var hashCount = 0;
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
@@ -48,7 +50,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     });
                     break;
                 }
-                if (seconds > 65,535) {
+                if (seconds > 65535) {
                     bot.sendMessage({
                         to: channelID,
                         message: 'the number of seconds is too large'
@@ -60,8 +62,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     message: 'Starting Timer for ' + seconds + ' seconds.'
                 });
-                let timeMilliseconds = seconds * 1000;
-                setTimeout(onTimer, timeMilliseconds, channelID);
+                addTimer(channelID, seconds);
 
                 break;
             // Just add any case commands if you want to..
@@ -69,9 +70,34 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     }
 });
 
-function onTimer(channelID) {
+function onTimer(arg) {
     bot.sendMessage({
-        to: channelID,
+        to: arg.channelID,
         message: 'Timer Elapsed'
     });
+    removeTimer(arg.channelID, arg.id);
+}
+
+function addTimer(channelID, seconds) {
+    if (hashCount > 1000) {
+        return -1;
+    }
+    let hashKey = getHashKey(channelID, hashCount);
+    timerHash[hashKey] = {};
+    let myId = hashCount;
+    hashCount++;
+    let timeMilliseconds = seconds * 1000;
+    setTimeout(onTimer, timeMilliseconds, { channelID: channelID, id: myId });
+    return myId;
+}
+
+function removeTimer(channelID, id) {
+    let hashKey = getHashKey(channelID, id);
+    delete timerHash[hashKey];
+    hashCount--;
+}
+
+function getHashKey(channelID, id) {
+    let hashKey = `channel${channelID}-${id}`;
+    return hashKey;
 }
